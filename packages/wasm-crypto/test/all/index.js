7feba3a5ea627cb4c1e1b71080d28ef87cf52320
kostiask <kostiaskopec@gmail.com>
2026-03-11 13:26:09 +0100

    test(wasm-crypto): add mldsa44 vectors and sign/verify coverage

--- packages/wasm-crypto/test/all/index.js
+++ packages/wasm-crypto/test/all/index.js
@@ -12,6 +12,7 @@ import * as secp256k1 from './secp256k1.js';
 import * as sr25519 from './sr25519.js';
 import * as vrf from './vrf.js';
 import * as dilithium2 from './dilithium2.js';
+import * as mldsa44 from './mldsa44.js';
 
 export const tests = {
   // We place secp256k1 first, this allows the interaction with it in the
@@ -24,7 +25,8 @@ export const tests = {
   hashing,
   sr25519,
   vrf,
-  dilithium2
+  dilithium2,
+  mldsa44
 };
 
 /**
